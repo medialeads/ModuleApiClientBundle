22222fc4e9bd624c08d35d493398e4045514eec9
chris <christopher.rochadias@gmail.com>
2018-01-30 09:48:54 +0100

    update Module Page

--- ModulePage/ModulePageClient.php
+++ ModulePage/ModulePageClient.php
@@ -4,8 +4,10 @@ namespace Module\ApiClientBundle\ModulePage;
 
 use Module\ApiClientBundle\Client\AbstractClient;
 use Module\ApiClientBundle\Model\Response;
+use Module\ApiCommonBundle\ModulePage\Request\_List;
 use Module\ApiCommonBundle\ModulePage\Request\Get;
 use Module\ApiCommonBundle\ModulePage\Request\GetTransformer;
+use Module\ApiCommonBundle\ModulePage\Request\ListTransformer;
 
 class ModulePageClient extends AbstractClient
 {
@@ -26,5 +28,23 @@ class ModulePageClient extends AbstractClient
         }
 
         return $this->apiHelper->get(sprintf('/api/module-page/%s/%s', $slug, $format), $format, GetTransformer::toParameters($get));
+    }
+
+    /**
+     * @param _List $list
+     * @param string $format
+     *
+     * @return Response
+     */
+    public function _list(_List $list, $format = 'json')
+    {
+        if (!in_array($format, array(
+            'json',
+            'xml'
+        ))) {
+            throw new \InvalidArgumentException();
+        }
+
+        return $this->apiHelper->get(sprintf('/api/module-page/list/%s', $format), $format, ListTransformer::toParameters($list));
     }
 }
